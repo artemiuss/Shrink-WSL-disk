eda723c2596df00a18f74c02142fcb72780c9ac7
artemiuss <artemiustas@gmail.com>
2021-07-20 16:23:00 +0300

    Update WSL shrink vhd.ps1

--- WSL shrink vhd.ps1
+++ WSL shrink vhd.ps1
@@ -5,9 +5,9 @@ $vhdPath = $env:userprofile + "\AppData\Local\Packages\" + $packageName
 $vhdPath = $vhdPath -replace "`t|`n|`r",""
 $vhdPath = $vhdPath + "\LocalState\ext4.vhdx"
 
-Write-Host "WSL2 disk size before shrinking:" + ((Get-Item $vhdPath).length/1GB) + "GB"
+Write-Host "WSL2 disk size before shrinking:" ((Get-Item $vhdPath).length/1GB) "GB"
 Write-Host "Shrinking WSL2 disk..."
 optimize-vhd -Path $vhdPath -Mode full
-Write-Host "WSL2 disk size before shrinking:" + ((Get-Item $vhdPath).length/1GB) + "GB"
+Write-Host "WSL2 disk size before shrinking:" ((Get-Item $vhdPath).length/1GB) "GB"
 
 wsl --startup
